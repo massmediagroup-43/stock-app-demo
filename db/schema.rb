4f1c4a7a6d5c58ce4662867efbbe111524d2ab09
Yaroslav Melnychuk <mmg.melnychuk.y@gmail.com>
2018-09-10 11:15:55 +0300

    implemented stock model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_10_073650) do
+ActiveRecord::Schema.define(version: 2018_09_10_080804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "name"
+    t.integer "duration"
+    t.integer "price"
+    t.float "interest"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_stocks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +38,5 @@ ActiveRecord::Schema.define(version: 2018_09_10_073650) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "stocks", "users"
 end
---
+++ spec/models/stock_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Stock, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    %i[price duration].each do |field|
+      it { should validate_numericality_of(field).is_greater_than_or_equal_to(1) }
+      it { should validate_numericality_of(field).only_integer }
+    end
+  end
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+  end
+end
